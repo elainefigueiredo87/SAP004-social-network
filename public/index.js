e041dad7e2372d47885ccc6be0364acba680efec
elainefigueiredo87 <elaine.figueiredo87@gmail.com>
2020-05-29 14:56:25 -0300

    organizacao das paginas

--- public/index.js
+++ public/index.js
@@ -1,4 +1,7 @@
 // Este é o ponto de entrada de sua aplicação
 import { home } from './pages/home/main.js';
+import { login } from './pages/login/main.js';
+import { register } from './pages/register/main.js';
+import { profile } from '.pages/profile/main.js';
 
 document.querySelector('#root').appendChild(home());
