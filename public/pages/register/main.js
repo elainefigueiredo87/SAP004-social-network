5379074f1cf8f50dcf7c06d8c1dcebe4b6b7e1ce
Jessica Brunhara <brunharaj@gmail.com>
2020-06-03 15:24:23 -0300

    css mobile first página register

--- public/pages/register/main.js
+++ public/pages/register/main.js
@@ -3,15 +3,28 @@ import { handleSignUp } from './data.js';
 export const register = () => {
   const signUp = document.createElement('div');
 
-  signUp.innerHTML = `<input type= "text" name= "name" id= "name" placeholder= "Nome Completo" required></input>
-    <br>
-    <input type= "email" name= "email" id= "email" placeholder= "Email" required></input>
-    <br>
-    <input type="password" name="password" id="password" placeholder= "Senha" required></input>
-    <br>
-    <input type="password" name="confirmPassword" id="confirmPassword" placeholder= "Confirme sua senha" required></input>
-    <br>
-    <button type="submit" id="btnRegister">Cadastrar</button>
+  signUp.className = ('signup-wrapper');
+
+  signUp.innerHTML = `
+  <div class='logo'> 
+    <img src='./images/wecanlogo.png' alt='logo'>
+  </div>
+  <div>
+    <input class='sign-login' type= "text" name= "name" id= "name" placeholder= "Nome Completo" required></input>
+  </div>
+  <div>  
+    <input class='sign-login' type= "email" name= "email" id= "email" placeholder= "Email" required></input>
+  </div>
+  <div>
+    <input class='sign-login' type="password" name="password" id="password" placeholder= "Senha" required></input>
+  </div>
+  <div>  
+    <input class='sign-login' type="password" name="confirmPassword" id="confirmPassword" placeholder= "Confirme sua senha" required></input>
+  </div>
+  <div class='message-error' id='message-error'></div>
+  <div> 
+    <button class='btn-login' type="submit" id="btnRegister">Cadastrar</button>
+  </div>  
     `;
 
   const btnRegister = signUp.querySelector('#btnRegister');
