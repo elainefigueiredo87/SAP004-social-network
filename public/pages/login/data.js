941ec952c858759e717614c6ed716bf33c48112f
Jessica Brunhara <brunharaj@gmail.com>
2020-06-11 10:29:27 -0300

    commit
    login pelo google redireciona para home

--- public/pages/login/data.js
+++ public/pages/login/data.js
@@ -38,3 +38,21 @@ export const signGoogle = () => {
       }
     });
 };
+
+/* função google antes de alterar
+export const signGoogle = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  return firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      const token = result.credential.accessToken;
+      const user = result.user;
+    })
+    .catch((error) => {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      let email = error.email;
+      let credential = error.credential;
+    });
+}; */
