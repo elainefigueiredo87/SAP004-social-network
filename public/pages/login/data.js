6f64819e707658b0103e121cae2478a466333d08
elainefigueiredo87 <elaine.figueiredo87@gmail.com>
2020-06-15 14:10:27 -0300

    refatoracao login

--- public/pages/login/data.js
+++ public/pages/login/data.js
@@ -10,49 +10,5 @@ export const signGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   return firebase
     .auth()
-    .signInWithPopup(provider)
-    .then((result) => {
-      // const token = result.credential.accessToken;
-      const user = {
-        firstName: result.additionalUserInfo.profile.given_name,
-        lastName: result.additionalUserInfo.profile.family_name,
-        email: result.user.email,
-      };
-      firebase
-        .firestore()
-        .collection('users')
-        .add(user);
-      window.location.href = '#home';
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      // const errorMessage = error.message;
-      // const email = error.email;
-      // const credential = error.credential;
-      if (errorCode === 'auth/account-exists-with-different-credential') {
-        alert('Você já se inscreveu com um provedor de autenticação diferente para esse email.');
-      } else {
-        console.error(error);
-        // } else if (error.email === 'auth/) {
-        //  alert
-      }
-    });
+    .signInWithPopup(provider);
 };
-
-/* função google antes de alterar
-export const signGoogle = () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  return firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then((result) => {
-      const token = result.credential.accessToken;
-      const user = result.user;
-    })
-    .catch((error) => {
-      let errorCode = error.code;
-      let errorMessage = error.message;
-      let email = error.email;
-      let credential = error.credential;
-    });
-}; */
