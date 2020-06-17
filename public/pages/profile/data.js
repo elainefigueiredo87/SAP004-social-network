60f8e9a3dcd877463711afab54f563325c614957
Jessica Brunhara <brunharaj@gmail.com>
2020-06-17 11:58:53 -0300

    agora menu hamburguer página home e profile

--- public/pages/profile/data.js
+++ public/pages/profile/data.js
@@ -1,1 +1,8 @@
-
+export const signOut = () => {
+  if (firebase.auth().currentUser) {
+    firebase.auth().signOut()
+      .then(() => {
+        window.location.href = '#login';
+      });
+  }
+};
